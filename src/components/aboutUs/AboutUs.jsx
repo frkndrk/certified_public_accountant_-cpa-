8246978e024bad930ee20f79286d99e7715a0896
FURKAN DURAK <furkandurak@FURKAN-MacBook-Air.local>
2023-12-14 15:31:11 +0300

    started aboutUs design

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -2,8 +2,87 @@ import "./aboutUs.scss"
 import React from 'react'
 
 const AboutUs = () => {
+  const services = [
+    {
+      id: 1,
+      name: "Tam Tasdik",
+      img: "../../src/assets/working-done.webp"
+    },
+    {
+      id: 2,
+      name: "KDV ile ÖTV İadesi ve Mahsup İşlemleri",
+      img: ""
+    },
+    {
+      id: 3,
+      name: "Vergi Uyuşmazlıkları",
+      img: ""
+    },
+    {
+      id: 4,
+      name: "Şirketler Hukuku Alanında",
+      img: ""
+    },
+    {
+      id: 5,
+      name: "Vergi ve Revizyon Konusunda Danışmanlık",
+      img: ""
+    },
+    {
+      id: 6,
+      name: "Vergi ve Muhasebe Yönetsel Danışmanlık",
+      img: ""
+    },
+    {
+      id: 7,
+      name: "Yönetsel Danışmanlık",
+      img: ""
+    },
+    {
+      id: 8,
+      name: "Diğer Denetim İşlemleri",
+      img: ""
+    },
+    {
+      id: 9,
+      name: "Diğer Tasdik İşlemleri",
+      img: ""
+    }
+  ]
+
   return (
-    <div className='aboutUs'>AboutUs</div>
+    <div className='aboutUs'>
+      <div className="contHeader">
+        <h2>MusEn YMM</h2>
+        <h1>Hizmet Alanlarımız</h1>
+      </div>
+      <div className="contCards">
+        <div className="contImg">
+          <img src={services[0].img} alt=""/>
+        </div>
+        <div className="contName">
+          <h3>{services[0].name}</h3>
+        </div>
+        <div className="contImg">
+          <img src={services[0].img} alt=""/>
+        </div>
+        <div className="contName">
+          <h3>{services[0].name}</h3>
+        </div>
+        <div className="contImg">
+          <img src={services[0].img} alt=""/>
+        </div>
+        <div className="contName">
+          <h3>{services[0].name}</h3>
+        </div>
+        <div className="contImg">
+          <img src={services[0].img} alt=""/>
+        </div>
+        <div className="contName">
+          <h3>{services[0].name}</h3>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,32 +1,50 @@
+import { useState } from "react"
 import "./navbar.scss"
 import CallIcon from '@mui/icons-material/Call'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const Navbar = () => {
 
+  const [navbar, setNavbar] = useState(false)
+
+  const scrollNum = () => {
+    if(window.scrollY >= 50) {
+      setNavbar(true)
+    } else {
+      setNavbar(false)
+    }
+  } 
+
+  window.addEventListener("scroll", scrollNum )
+
   
   return (
-    <div className="navbar" >
+    <div className={navbar ? "navbar active" : "navbar"} style={{position: navbar && "fixed", top: navbar && "0"}}>
       <div className="cont1">
-        <div className="logo">
-          <h1>MusEn</h1>
-          <p>Güvenle</p>
+        <div className={navbar ? "logo fontColor" : "logo"}>
+          <h1 className={navbar && "borderBottom"}>MusEn</h1>
+          <p className={navbar && "fontColor"}>Güvenle</p>
         </div>
         <div className="menu">
-          <ul>
-            <li>Anasayfa</li>
-            <li>Kurumsal</li>
-            <li>Hizmetlerimiz</li>
-            <li>Mevzuat</li>
-            <li>Haberler</li>
-            <li>İletişim</li>
+          <ul className="menuUl">
+            <li className={navbar ? "menuLi fontColor" : "menuLi"}>Anasayfa</li>
+            <li className={navbar ? "menuLi fontColor" : "menuLi"} style={{display: "flex", position: "relative"}}>Kurumsal<KeyboardArrowDownIcon />
+              <ul style={{position: "absolute", top: "100px", left: "0", listStyle: "none", textDecoration: "none", backgroundColor: "#fff", borderRadius: "5px", padding: "10px"}}>
+                <li style={{margin: "10px 20px", borderBottom: "1px solid #aaa", paddingBottom: "15px", color: "rgba(59, 59, 61, 0.8)"}}>Hakkımızda</li>
+                <li style={{margin: "10px 20px", color: "rgba(59, 59, 61, 0.8)"}}>Ortaklarımız</li>
+              </ul>
+            </li>
+            <li className={navbar ? "menuLi fontColor" : "menuLi"}>Hizmetlerimiz</li>
+            <li className={navbar ? "menuLi fontColor" : "menuLi"}>Mevzuat</li>
+            <li className={navbar ? "menuLi fontColor" : "menuLi"}>İletişim</li>
           </ul>
         </div>
         <div className="contact">
           <ul>
             <li><CallIcon style={{backgroundColor: "#fff", color: "black", borderRadius: "50%", padding: "10px"}} /></li>
             <li>
-              <p>Sabit Hattımız</p>
-              <p>444 01 01</p>
+              <p className={navbar && "fontColor"}>Sabit Hattımız</p>
+              <p className={navbar && "fontColor"}>444 01 01</p>
             </li>
           </ul>
         </div>
