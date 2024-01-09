75a1a811eaae664e6279cef82cb9531ba40e8260
FURKAN DURAK <furkandurak@FURKAN-MacBook-Air.local>
2024-01-09 16:07:55 +0300

    finished all pages

--- src/pages/contactPage/ContactPage.jsx
+++ src/pages/contactPage/ContactPage.jsx
@@ -8,9 +8,64 @@ import AlignVerticalTopIcon from '@mui/icons-material/AlignVerticalTop';
 import plaza from "../../assets/plaza3.webp";
 import buildImg from "../../assets/build21.jpeg";
 import callservice from "../../assets/callservice.jpg";
+import { useState } from "react";
+import CropSquareIcon from '@mui/icons-material/CropSquare';
+import HexagonIcon from '@mui/icons-material/Hexagon';
+import FileDownloadDoneIcon from '@mui/icons-material/FileDownloadDone';
 
 
 const ContactPage = () => {
+
+  const [btnState, setBtnState] = useState(false)
+  const [btnTxt, setBtnTxt] =  useState("Gönder")
+  const [icon, setIcon] = useState(<HexagonIcon />)
+  const [focused, setFocused] = useState(false)
+
+  const [values, setValues] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    phone: "",
+    message: ""
+  })
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    if(values.phone === "" || values.email ==="") {
+      alert("Lütfen istenilen bilgileri doldurunuz!")
+    } else {
+      setBtnState(true)
+      setBtnTxt("Gönderiliyor...")
+      setIcon(<HexagonIcon />)
+      setTimeout(() => {
+        setBtnState(false)
+        setBtnTxt("Gönderildi")
+        setIcon(<FileDownloadDoneIcon />)
+        setValues({
+          name: "",
+          surname: "",
+          email: "",
+          phone: "",
+          message: ""
+        })
+      }, 3000);
+    }
+
+    
+
+    const formData = new FormData(e.target)
+    const payLoad = Object.fromEntries(formData)
+  }
+
+  const onChange = (e) => {
+    setValues({...values, [e.target.name]: e.target.value})
+  }
+
+  const handleFocus = (e) => {
+    setFocused(true)
+  }
+
   return (
     <div className='contactPage'>
         <div className="imgContB1">
@@ -79,14 +134,14 @@ const ContactPage = () => {
           <h2>EnKa YMM</h2>
           <h1>İletişim Formu</h1>
           <div className="form">
-            <form action="">
-              <input type="text" name="name" placeholder="Adınız"/>
-              <input type="text" name="surname" placeholder="Soyadınız"/>
-              <input type="email" name="email" placeholder="E-Mail Adresiniz"/>
-              <input type="tel" name="phone" placeholder="Telefon Numaranız"/>
-              <textarea name="" id="" cols="30" rows="15" placeholder="Mesajınız"></textarea>
+            <form onSubmit={handleClick}>
+              <input onChange={onChange} type="text" name="name" placeholder="Adınız" value={values.name}/>
+              <input onChange={onChange} type="text" name="surname" placeholder="Soyadınız" value={values.surname}/>
+              <input onChange={onChange} type="email" name="email" placeholder="E-Mail Adresiniz" value={values.email} pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" onBlur={handleFocus} focused={focused.toString()}/>
+              <input onChange={onChange} type="tel" name="phone" placeholder="Telefon Numaranız" value={values.phone} pattern="^[0-9]{11}$" onBlur={handleFocus} focused={focused.toString()}/>
+              <textarea onChange={onChange} name="message" cols="30" rows="15" placeholder="Mesajınız" value={values.message}></textarea>
               <div className="btnFC">
-                <button className="btn-fc3">Gönder</button>
+                <button className={btnState ? "btn-fc3 active" : "btn-fc3"}><span className={btnState && "activeIcon"}>{icon}</span>{btnTxt}</button>
               </div>
             </form>
           </div>
