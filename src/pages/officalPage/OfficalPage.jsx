8777e3ba1e0f496841b9d43de57378f48532075f
FURKAN DURAK <furkandurak@FURKAN-MacBook-Air.local>
2024-01-15 10:35:31 +0300

    last update

--- src/pages/officalPage/OfficalPage.jsx
+++ src/pages/officalPage/OfficalPage.jsx
@@ -26,9 +26,9 @@ const OfficalPage = () => {
             <p>Enka dürüstlüğü, güvenirliliği, ulaşılabilirliği ve dinamizmi ilke edinerek müşterilerimize ve paydaşlarımıza hizmet vermeyi , sunmuş olduğu hizmetlerin kalitesinde evrensel düzeyi yakalamayı amaç olarak belirlemiştir. Bu kapsamda genç, dinamik , vizyoner ve uzman kadrolarımız ile müşteri ve paydaşlarımızın ticari hayatlarına katma değer yaratarak hizmet vermeyi hedeflemektedir.</p>
             <p>EnKa enerji, inşaat, finans, turizm, medya, yazılım, e-ticaret, sağlık, ulaştırma, gıda, otomotiv, tekstil, teknoloji, kamu özel altyapı projeleri başta olmak üzere, farklı sektör ve ölçekte çok sayıda lider şirketin ihtiyaçlarına uygun hizmetler üreten çözüm ortağıdır.</p>
           </div>
-          <div className="contImgB1">
+          {/* <div className="contImgB1">
             <img src={buildOffical2} alt="Enka, enkaymm, Enka YMM, enka ymm, Enka Ymm, enes karakurt, Enes Karakurt, Enes Karakurt YMM, Enes Karakurt Ymm, Enes Karakurt Yeminli Mali Müşavir, ymm, YMM, Ymm, Yeminli, Mali, Müşavir, Yeminli Mali Müşavir, Musavir, Ankara Ymm, Ankara Yeminli Mali Müşavir, Ankara enka, ankara enka, Ankara Enka, Tasdik, Denetim, İade, iade, vergi incelemesi , vergi danışmanlığı, Vergi, vergi iadesi, kdv iade, danışmanlık" />
-          </div>
+          </div> */}
         </div>
         <div className="contB1">
           <img className="contB11" src={hexagon} alt="Enka, enkaymm, Enka YMM, enka ymm, Enka Ymm, enes karakurt, Enes Karakurt, Enes Karakurt YMM, Enes Karakurt Ymm, Enes Karakurt Yeminli Mali Müşavir, ymm, YMM, Ymm, Yeminli, Mali, Müşavir, Yeminli Mali Müşavir, Musavir, Ankara Ymm, Ankara Yeminli Mali Müşavir, Ankara enka, ankara enka, Ankara Enka, Tasdik, Denetim, İade, iade, vergi incelemesi , vergi danışmanlığı, Vergi, vergi iadesi, kdv iade, danışmanlık" />
@@ -38,7 +38,7 @@ const OfficalPage = () => {
             </div>
             <div className="contB1A2">
               <h4 className="contB1A21">EnKa YMM</h4>
-              <h1 className="contB1A22">ENKA YMM ve Danışmanlık A.Ş Olarak Sizlerle Büyümeye Devam Ediyoruz</h1>
+              <h1 className="contB1A22">ENKA YMM Olarak Sizlerle Büyümeye Devam Ediyoruz</h1>
             </div>
           </div>
         </div>
